b571b8e938cdb6c1fdca05417b257c3a0e218c13
bogzbonny <bogzbonny@protonmail.com>
2025-03-06 04:47:42 +0000

    fix: simplify clap usage and add serde for data serialization/deserialization

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use clap::{App, Arg, SubCommand};
-use clap::{Arg, Command}; // Updated clap structure
+use clap::Command;
+use serde::{Deserialize, Serialize};
 use std::fs;
 use std::path::PathBuf;
 use std::{thread, time};
